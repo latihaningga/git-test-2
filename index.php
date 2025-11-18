bdf8f09ed2a404ada6edf30255f078063bcc00bf
ingga <widjayaingga@gmail.com>
2025-11-18 13:37:31 +0700

    menambah list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis doloribus aut esse rerum, quod recusandae laborum omnis quae in accusamus fugit neque, sit pariatur aperiam voluptatum tempore eum a placeat?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
